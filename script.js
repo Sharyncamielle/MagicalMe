8bb374c64032ed9e33dc96281d2a167ddfb6306a
Sharyncamielle <143422283+Sharyncamielle@users.noreply.github.com>
2023-09-27 10:05:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function palmLeaf(){
+  var password = document. getElementById("palmTree");
+  var passwordText = password.value;
+  if(passwordText=="NY") {
+    return true;
+  }
+  alert("Try again")
+  return fale;
+}
